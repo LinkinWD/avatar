40d33067ede9ad7a918a4316ce6f3ef54963a11e
Jarmo Lindström <yaramodesu@gmail.com>
2022-09-14 19:33:21 +0300

    teksti vaihtuu, mutta tarttee parannusta

--- components/Cta.js
+++ components/Cta.js
@@ -1,15 +1,32 @@
+import { useEffect, useState } from 'react';
+
 import styles from '../styles/Cta.module.css';
 
 import { useGlobalContext } from '../context';
 
 const Cta = () => {
+	const sanat = [ 'Scrollaa', 'Alas', 'Tai', 'paina', 'minua' ];
+	const [ currentWord, setCurrentWord ] = useState('');
+	let luku = 0;
+	useEffect(() => {
+		const ajastin = setInterval(() => {
+			setCurrentWord(sanat[luku]);
+			console.log(currentWord);
+			luku++;
+			if (luku === 5) {
+				luku = 0;
+			}
+		}, 1500);
+		return () => clearInterval(ajastin);
+	}, []);
+
 	//scrollRef on globaali, käytetään kahdessa componentissa. CTA ja Main
 	const { scrollRef } = useGlobalContext();
 	const executeScroll = () => scrollRef.current.scrollIntoView();
 
 	return (
 		<button onClick={executeScroll} className={styles.btn}>
-			Tutustu
+			{currentWord}
 		</button>
 	);
 };
